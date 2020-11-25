76e3f81f5f4ff23756e0eb3a204950a0bd0d6e96
Malik <malik43213@gmail.com>
2020-11-25 14:56:30 +0100

    add wallet route

--- src/API/Routes/index.js
+++ src/API/Routes/index.js
@@ -1,13 +1,15 @@
-const userRoutes = require('./userRoutes');
-const orderRoutes = require('./orderRoutes');
 const messageRoutes = require('./messageRoutes');
 const notFoundRoute = require('./notFoundRoute');
+const orderRoutes = require('./orderRoutes');
 const responseHandler = require('../../Middlewares/responseHandler');
+const userRoutes = require('./userRoutes');
+const walletRoutes = require('./walletRoutes');
 
 module.exports = (app) => {
   app.use('/users', userRoutes);
   app.use('/orders', orderRoutes);
   app.use('/messages', messageRoutes);
+  app.use('/wallet', walletRoutes);
   app.use('/*', notFoundRoute);
   app.use(responseHandler);
 };
